46c7e70e1eb461c7848677efd2384677857d1bd2
niteshpawar97 <niteshpawar97@gmail.com>
2023-10-23 01:07:49 +0530

    some changes

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/shopUserRoutes.js
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/authController');
-
-router.get('/', AuthController.getAllUsers);
-router.post('/', AuthController.createUser);
-
-module.exports = router;
